a7af051310508d887a7ceda53eafaf36cfcc95d0
nft-fun <80271565+nft-fun@users.noreply.github.com>
2021-11-05 23:23:05 -0600

    Create query_sale_events.py
    ;)

---
+++ opensea_utilities/query_sale_events.py
@@ -0,0 +1,19 @@
+import requests
+import time
+import json
+
+r = requests.get('https://api.opensea.io/api/v1/events?collection_slug=bit-birds&event_type=successful&limit=20')
+jsonResponse = r.json()
+
+for i in range(20):
+    print('\n\n----{}----'.format(i))
+    name = jsonResponse['asset_events'][i]['asset']['name']
+    timestamp = jsonResponse['asset_events'][i]['created_date']
+    amount = int(jsonResponse['asset_events'][i]['total_price'])
+    eth = amount / 1000000000000000000
+    winner = jsonResponse['asset_events'][i]['winner_account']['address']
+    winner_username = jsonResponse['asset_events'][i]['winner_account']['user']['username']
+
+    print('Name: {}\nTime: {}\nAmount: {} ETH\nWinner: {}\nUsername: {}'.format(name, timestamp, eth, winner, winner_username))
+
+print('\n\n---Done---')
